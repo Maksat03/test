810345be4184b4924b6f8442ff57057aafd6d5fd
Maksat <54549462+Maksat03@users.noreply.github.com>
2023-07-15 20:23:37 +0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,19 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Максат Кантай on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//qwe
